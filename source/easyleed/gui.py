andim/easyleed

commit b5103a0b7e7f32ab7ff7981eab3de6c9811ad309

energy label now as a SimpleTextItem

source/easyleed/gui.py
@@ -14,7 +14,7 @@ from PyQt4.QtCore import (QPoint, QRectF, QPointF, Qt, SIGNAL, QTimer, QObject)
 from PyQt4.QtGui import (QApplication, QMainWindow, QGraphicsView,
     QGraphicsScene, QImage, QWidget, QHBoxLayout, QPen, QSlider,
     QVBoxLayout, QPushButton, QGraphicsEllipseItem, QGraphicsRectItem, QGraphicsItem,
-    QGraphicsTextItem,
+    QGraphicsSimpleTextItem,
     QPainter, QKeySequence, QAction, QIcon, QFileDialog, QProgressBar, QAbstractSlider,
     QBrush, QFrame, QLabel, QRadioButton, QGridLayout, QSpinBox, QDoubleSpinBox, QCheckBox,
     QComboBox, QLineEdit, QMessageBox, QPixmap)
@@ -235,9 +235,12 @@ class GraphicsScene(QGraphicsScene):
     def setBackground(self, image, labeltext):
         """ Sets the background image. """
         if not hasattr(self, 'imlabel'):
-            self.imlabel = QGraphicsTextItem(labeltext)
+            self.imlabel = QGraphicsSimpleTextItem(labeltext)
+            self.imlabel.setBrush(QBrush(Qt.white))
+            #self.imlabel.setPen(QPen(Qt.white))
+            self.imlabel.setPos(5, 5)
             self.addItem(self.imlabel)
-        self.imlabel.setPlainText(labeltext)
+        self.imlabel.setText(labeltext)
         self.image = image
         self.update()
     

commit 300d4ea8f925583fb80c84a957f5bb1850c338ca

Fixed removal of spots action.
It was broken in commit b5103a0 upstream.

source/easyleed/gui.py
@@ -778,9 +778,9 @@ class MainWindow(QMainWindow):
                 self.statusBar().showMessage('IOError: ' + str(err), 5000)
 
     def removeLastSpot(self):
-        if len(self.scene.items(1)) != 0:
+        if len(self.scene.items(0)) > 1:
             self.scene.spots.remove(self.scene.spots[-1])
-            self.scene.removeItem(self.scene.items(1)[-1])
+            self.scene.removeItem(self.scene.items(0)[-1])
 
     def stopProcessing(self):
         self.stopped = True