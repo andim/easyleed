d78fb64cbe2d0dc66808f96c695a061991cdcebb
Andreas Mayer <andisspam@gmail.com>
2014-10-03 14:02:28 +0200

    fixed bug causing fitRegionFactor to not be used and refactoring

--- source/easyleed/base.py
+++ source/easyleed/base.py
@@ -127,14 +127,15 @@ def guesser(npimage, x_in, y_in, radius, func = eval(config.Tracking_guessFunc),
         logger.info("no guess, because " + reason)
         print reason
         return None
+
+    # try to get patch from image around estimated position
     try:
-        fit_region_factor = 1.0
         x_min, x_max, y_min, y_max = adjust_slice(npimage, x_in - fit_region_factor * radius, x_in + fit_region_factor * radius + 1,
                                      y_in - fit_region_factor * radius, y_in + fit_region_factor * radius + 1)
     except IndexError:
        return failure("position outside image")
-   
     image = npimage[y_min : y_max, x_min : x_max]
+
     result = func(image, x_mid = x_in - x_min, y_mid = y_in - y_min, size = radius)
     if result is None:
         return failure("fit failed")
@@ -142,7 +143,7 @@ def guesser(npimage, x_in, y_in, radius, func = eval(config.Tracking_guessFunc),
     y_res, x_res = pos
     x_res += x_min
     y_res += y_min
-    
+
     return x_res, y_res, cov
     
 def gaussian2d(height, center_x, center_y, width_x, width_y = None,
@@ -215,16 +216,16 @@ def calc_distances(shape, x, y, squared = True):
     squared: return the squared distance (default: True)
     """
     yind, xind = np.indices(shape)
-    distances = ((yind - y)**2 + (xind - x)**2)
+    distSquare = ((yind - y)**2 + (xind - x)**2)
     if not squared:
-        distances = distances**.5
-    return distances
+        return distSquare**.5
+    return distSquare
 
 def signal_to_background(npimage, x, y, radius):
-    distances = calc_distances(npimage.shape, x, y)
-    signal = np.mean(npimage[distances <= radius**2])
+    distSquare = calc_distances(npimage.shape, x, y)
+    signal = np.mean(npimage[distSquare <= radius**2])
     # average background intensity over annulus with equal area
-    background = np.mean(npimage[np.logical_and(distances >= radius**2, distances <= 2 * radius**2)])
+    background = np.mean(npimage[np.logical_and(distSquare >= radius**2, distSquare <= 2 * radius**2)])
     return signal/background
   
 def calc_intensity(npimage, x, y, radius, background_substraction=config.Processing_backgroundSubstractionOn):
@@ -235,12 +236,12 @@ def calc_intensity(npimage, x, y, radius, background_substraction=config.Process
         radius: radius of the spot
         background_substraction: boolean to turn substraction on/off
     """
-    yind, xind = np.indices(npimage.shape)
-    distances = ((yind - y)**2 + (xind - x)**2)
-    intensities = npimage[distances <= radius**2]
-    intensity, area = np.sum(intensities), len(intensities)
+    distSquare = calc_distances(npimage.shape, x, y, squared = True)
+    intensities = npimage[distSquare <= radius**2]
+    intensity = np.sum(intensities)
     if background_substraction:
         # average background intensity over annulus with approximately equal area
-        background_intensities = npimage[np.logical_and(distances >= radius**2, distances <= 2 * radius**2)]
+        background_intensities = npimage[np.logical_and(distSquare >= radius**2, distSquare <= 2 * radius**2)]
+        area = len(intensities)
         intensity -= np.mean(background_intensities) * area
     return intensity
