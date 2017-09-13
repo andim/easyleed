96eab9c5e1d8b5ecb934afff0bc36ccc98a5be5d
Andreas Mayer <andisspam@gmail.com>
2017-09-13 19:06:39 +0200

    version bumped to 2.3.3

--- source/setup.py
+++ source/setup.py
@@ -6,7 +6,7 @@ setup(
     requires=['numpy', 'matplotlib', 'scipy', 'pillow', 'pyqt4'],
 #    scripts=['easyleed.pyw'],
     entry_points={'gui_scripts' : ['easyleed = easyleed.__main__:main']},
-    version='2.3.2',
+    version='2.3.3',
     description='Automated extraction of intensity-energy spectra from low-energy electron diffraction patterns',
     long_description= """
 EasyLEED facilitates data analysis of images obtained by low-energy electron diffraction, a common technique in surface science. It aims to automate the process of extracting intensity-energy spectra from a series of diffraction patterns acquired at different beam energies. At its core a tracking algorithm exploiting the specifics of the underlying physics (see `paper <http://dx.doi.org/10.1016/j.cpc.2012.02.019>`_) allows to link the position of the diffraction maxima between subsequent images.
@@ -16,7 +16,7 @@ For more info please see https://andim.github.io/easyleed/ or contact the author
     author='Andreas Mayer, Hanna Salopaasi, Nicola Ferralis',
     author_email='andimscience@gmail.com',
     url='https://andim.github.io/easyleed/',
-    download_url='https://github.com/andim/easyleed/archive/2.3.2.tar.gz',
+    download_url='https://github.com/andim/easyleed/archive/2.3.3.tar.gz',
     keywords=['LEED', 'surface science', 'image analysis', 'I(E) spectra', 'spot tracking'],
     license='GPLv2',
     platforms='any',
