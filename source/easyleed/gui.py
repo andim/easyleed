5afdf0a83f91469b4fabbb44f12d58840fac324b
Nicola Ferralis <feranick@hotmail.com>
2017-11-06 14:47:27 -0500

    Capture both IOError and StopIteration exception while opening files

--- source/easyleed/gui.py
+++ source/easyleed/gui.py
@@ -1044,8 +1044,10 @@ class MainWindow(QMainWindow):
         try:
             self.loader = AllImageLoader(files, config.IO_energyRegex)
             self.setImage(self.loader.next())
+        except IOError as err:
+            self.statusBar().showMessage('IOError: ' + str(err), 5000)
         except StopIteration:
-            self.statusBar().showMessage('Error opening image files', 5000)
+            self.statusBar().showMessage('No image file selected', 5000)
         else:
             self.enableProcessActions(True)
             self.prevButton.setEnabled(True)
